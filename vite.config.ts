f3472b07d429a4f9cd4a52762b949c357288c314
younexxx <unas_prime@outlook.com>
2025-02-27 20:16:53 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/UNAX-WEB",
+  base: "/unax.github.io",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
